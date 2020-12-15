8352a86793c3073120e637fa51f24ce72bda200c
ardi_nugraha <ardi.nugraha@meteor.id>
2020-12-15 18:13:18 +0700

    refactor files

---
+++ vechile_counter/__init__.py
@@ -0,0 +1,16 @@
+from flask import Flask
+from flask_mysqldb import MySQL
+
+app = Flask(__name__)
+
+app.config['SECRET_KEY'] = 'ardi'
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = 'ardi32145'
+app.config['MYSQL_DB'] = 'websitedishub'
+
+mysql = MySQL(app)
+
+cur = mysql.connection.cursor()
+
+from vechile_counter import routes
--- vechile_counter/routes.py
+++ vechile_counter/routes.py
@@ -1,20 +1,11 @@
-import time
-import cv2 
-import numpy as np
 from flask import Flask, render_template, Response
-from imutils.video import VideoStream
-from imutils.video import FPS
-import argparse
-import imutils
-import time
-import dlib
-from time import sleep
-from ardi_utils import car_counting
+from vechile_counter.ardi_utils import car_counting
+from vechile_counter import app, mysql
 
-app = Flask(__name__)
 
 @app.route("/video_feed", methods=['GET'])
 def video_feed():
+
     """Video streaming route. Put this in the src attribute of an img tag."""
     global car, global_frame
     v = car_counting.vechile()
@@ -24,7 +15,3 @@ def video_feed():
 @app.route('/')
 def index():
     return render_template('index.html')
-
-
-if __name__ == "__main__":
-    app.run(debug=True)
